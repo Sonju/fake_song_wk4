44378e831b40fcf5f3f90e7aa7e0178149d74954
Sonju Walker <sonjuwalker@gmail.com>
2015-07-24 17:22:01 -0500

    #2 finished

--- app.rb
+++ app.rb
@@ -3,11 +3,24 @@ Bundler.require()
 
 
 def fake_song (title, release_year, artist)
-  {:title => 'title', :release_year => 'release_year', :artist => 'artist'}
+  {:title => title, :release_year => release_year, :artist => artist}.to_json
 end
 
 get '/' do
   "Welcome to Fake Songs Website!"
 end
 
-get 
+get '/api/Billy-Jean' do
+  cross_origin
+  fake_song('Billy Jean', '1980', 'Micheal Jackons')
+end
+
+get '/api/Whisper-Song' do
+  cross_origin
+  fake_song('Whisper Song', '1984', 'Frankie Knuckles')
+end
+
+get '/api/Purple-Rain' do
+  cross_origin
+  fake_song('Purple Rain', '1982', 'Prince')
+end
